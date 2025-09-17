27c42ecb779218423ba059c9f8d70495e07a6079
Lia <29ldivcohen@gannacademy.org>
2025-09-17 09:09:51 -0400

    done

--- src/App.java
+++ src/App.java
@@ -10,7 +10,29 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
+        
+        int evencount = 0;
+        int oddcount = 0;
 
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+             if (number== -1) {
+                break;
+                
+            }
+         if (number%2==0){
+            System.out.println("This number is even");
+            evencount++;
 
+        }else{ 
+            System.out.println("This number is odd");
+          oddcount++;
+        
+        } 
+       
+        }
+        System.out.println("There was " + (oddcount + evencount));
     }
-}
+    }
+
+    
